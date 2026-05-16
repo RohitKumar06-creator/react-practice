191a696c4351112be890054af843435901319f35
Rohit Kumar <rhgoswami13@gmail.com>
2026-05-16 20:37:56 +0500

    Second Project

---
+++ 02-Components/src/components/card.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Card = () => {
+    return (
+        <div>
+            <div className="card">
+                <h2>Card Component</h2>
+                <p>This is a simple card component.</p>
+            </div>
+        </div>
+    );
+};
+
+export default Card;   
