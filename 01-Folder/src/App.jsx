d5af0c038386804cfe9efb6214ee5952640f054a
Rohit Kumar <rhgoswami13@gmail.com>
2026-05-16 18:23:36 +0500

    First React Project

---
+++ 01-Folder/src/App.jsx
@@ -0,0 +1,16 @@
+const App = () => {
+  return (
+    <>
+      <div id="parent">
+        <h1 id="child1">Welcome to My App</h1>
+        <p id="child2">This is a simple React application.</p>
+      </div>
+
+      <div id="uncle">
+        <h2 id="cousin1">Hello, I'm the uncle!</h2>
+      </div>
+    </>
+  );
+}
+
+export default App; 
