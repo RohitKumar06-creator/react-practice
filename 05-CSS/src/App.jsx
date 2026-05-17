27c39e3ef9927e2f48d324ea302108ec46bb4969
Rohit Kumar <rhgoswami13@gmail.com>
2026-05-17 05:34:26 +0500

    CSS Practice

---
+++ 05-CSS/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Button from './components/button/Button.jsx'
+import Header from './components/header/Header.jsx'
+import './index.css'
+
+const App = () => {
+  return (
+    <div>
+      <Button />
+      <Header />
+    </div>
+  )
+}
+
+export default App
