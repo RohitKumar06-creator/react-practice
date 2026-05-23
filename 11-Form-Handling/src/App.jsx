4bd20a727f62f2f654d6f1297816390df1f73b29
Rohit Kumar <rhgoswami13@gmail.com>
2026-05-24 02:48:01 +0500

    Forms Handling

---
+++ 11-Form-Handling/src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const App = () => {
+
+  const submitHandler = (event) => {
+    event.preventDefault()
+    console.log('Form submitted')
+  }
+
+  return (
+    <div>
+      <form onSubmit={(event) => {
+        submitHandler(event)
+      }}>
+        <input type="text" placeholder="Enter your name" />
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  )
+}
+
+export default App
